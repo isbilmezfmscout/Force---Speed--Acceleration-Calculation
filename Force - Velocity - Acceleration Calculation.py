313a790f23540e584148e5322aef295e67847eee
Mehmet Emin Kaya <99548760+isbilmezfmscout@users.noreply.github.com>
2022-02-16 23:53:32 +0300

    Add files via upload

---
+++ Force - Velocity - Acceleration Calculation.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 00:58:55 2022
+
+@author: mehme
+"""
+
+
+   
+def formul():
+    yerd = int(input("Yer Değiştirmeyi Girin: "))
+    t = int(input("Zamanı Girin: "))
+    m = int(input("Kütleyi Girin: "))
+    hiz = yerd / t
+    a = hiz / t
+    F = m*a
+    print("Hız: ", hiz)
+    print("İvme: ", a)
+    print("Kuvvet: ", F)
+    
+formul()
+
+   ######################     Mehmet Emin Kaya  ####################### 
